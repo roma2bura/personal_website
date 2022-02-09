a51f9ddf03e37ce2c29b8fad1910d8e9edb3cf6f
Roman Bura <r2bcloud@gmail.com>
2022-02-08 23:14:25 -0800

    init

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'R2B.DEV'"; //letter subject
+	$to = 'r2bcloud@gmail.com'; // Enter Your E-mail
+	$from = 'roman@r2b.team'; // Admin e-mail
+	$fromName = 'Roman Bura Resume'; // Your company name
+	$charset = 'UTF-8';
+?>
